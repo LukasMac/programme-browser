f6c5f21fda224599c79f143067c4cb23843d343a
Lukas Maciulis <lukas.maciulis@gmail.com>
2015-02-07 18:00:45 +0000

    Refactor Programme spec

--- spec/models/programme_spec.rb
+++ spec/models/programme_spec.rb
@@ -11,24 +11,25 @@ RSpec.describe Programme, :type => :model do
     }
   end
 
+  before do
+    @programme = Programme.new(programme_hash)
+  end
+
   describe '#name' do
     it 'should return correct programme name' do
-      programme = Programme.new(programme_hash)
-      expect(programme.name).to eq 'programme_name'
+      expect(@programme.name).to eq 'programme_name'
     end
   end
 
   describe '#description' do
     it 'should return correct programme description' do
-      programme = Programme.new(programme_hash)
-      expect(programme.description).to eq 'programme_description'
+      expect(@programme.description).to eq 'programme_description'
     end
   end
 
   describe '#image' do
     it 'should return correct programme image' do
-      programme = Programme.new(programme_hash)
-      expect(programme.image).to eq 'image.jpg'
+      expect(@programme.image).to eq 'image.jpg'
     end
   end
 end
