17a303aed3489006730d6487b3f393a0a01c98d5
Lukas Maciulis <lukas.maciulis@gmail.com>
2015-02-08 22:49:01 +0000

    Refactor VideoAsset spec

--- spec/models/video_asset_spec.rb
+++ spec/models/video_asset_spec.rb
@@ -10,38 +10,37 @@ RSpec.describe VideoAsset, :type => :model do
     }
   end
 
+  before do
+    @video_asset = VideoAsset.new(video_asset_hash)
+  end
+
   describe '#id' do
     it 'should return correct id number' do
-      video_asset = VideoAsset.new(video_asset_hash)
-      expect(video_asset.id).to eq '123'
+      expect(@video_asset.id).to eq '123'
     end
   end
 
   describe '#title' do
     it 'should return correct title' do
-      video_asset = VideoAsset.new(video_asset_hash)
-      expect(video_asset.title).to eq 'Video title'
+      expect(@video_asset.title).to eq 'Video title'
     end
   end
 
   describe '#description' do
     it 'should return correct description' do
-      video_asset = VideoAsset.new(video_asset_hash)
-      expect(video_asset.description).to eq 'Video description'
+      expect(@video_asset.description).to eq 'Video description'
     end
   end
 
   describe '#image' do
     it 'should return correct image path' do
-      video_asset = VideoAsset.new(video_asset_hash)
-      expect(video_asset.image).to eq 'http://image.jpg'
+      expect(@video_asset.image).to eq 'http://image.jpg'
     end
   end
 
   describe '#video' do
     it 'should return correct video path' do
-      video_asset = VideoAsset.new(video_asset_hash)
-      expect(video_asset.video('idol')).to eq 'http://www.tv4play.se/program/idol?video_id=123'
+      expect(@video_asset.video('idol')).to eq 'http://www.tv4play.se/program/idol?video_id=123'
     end
   end
 end
